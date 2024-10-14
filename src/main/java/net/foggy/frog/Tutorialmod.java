9ee8dc809fd7d2476c7ae728fda99a9c6c969559
DED33L <tomyouri@hotmail.com>
2024-10-14 17:02:47 +0200

    Initial commit

---
+++ src/main/java/net/foggy/frog/Tutorialmod.java
@@ -0,0 +1,16 @@
+package net.foggy.frog;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorialmod implements ModInitializer {
+	public static final String MOD_ID = "frog";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/foggy/frog/frogclient.java
@@ -0,0 +1,10 @@
+package net.foggy.frog;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class frogclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
